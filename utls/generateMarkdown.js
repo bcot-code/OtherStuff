60a5bb365d3c7b6a45dc3d4de7e03f5196762790
bcot-code <78989054+bcot-code@users.noreply.github.com>
2023-11-19 17:57:09 -0600

    fixed README sample

--- utls/generateMarkdown.js
+++ utls/generateMarkdown.js
@@ -1,10 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license === "None") {
-    return "";
-  } else {
+  if (license !== "No license") {
     return `![GitHub License](https://img.shields.io/badge/license-${license}-blue)`;
+  } else {
+    return "";
   }
 }
 
@@ -14,78 +14,82 @@ function renderLicenseLink(license) {
   if (license === "None") {
     return "";
   } else {
-    return `(<a href="http://choosealicense.com/licenses/${license}/">${renderLicenseBadge}
-            .replace(/ /g,"%20")}</a>)`;
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license === "None") {
-    return "";
+  if (license !== "None") {
+    return `## License (#table-content)
+    This app is covered by :
+    ${renderLicenseLink(license)}`;
   } else {
-    return `## License ${renderLicenseLink}`;
+    return ``;
   }
 }
-
+function renderLicenseTOC(license) {
+  if (license !== "None") {
+    return `- [License](#license)`;
+  } else {
+    return "";
+  }
+}
 // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
-
-//     ## Description
-//     ${data.description}
-//     ## Table of Contents
-//     ${data.contents}
-//     ## Installation
-//     ${data.installation}
-//     ## Usage
-//     ${data.usage}
-//     ## License
-//     This app is covered by ${data.license}.
-//     ## Contributing
-//     ${data.contribution}
-//     ## Tests
-//     ${data.test}
-//     ## Questions
-//     ${data.questions} to ${data.github} or via ${data.email}
-
-// `;
-// }
 function generateMarkdown(data) {
   return `
   # ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-## Description
+
+
+## Table-of-Contents
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseTOC(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- ${data.contents}
+
+
+## Description(#table-of-contents)
 
 Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
 ${data.description}
 
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- ${data.contents}
-
-## Installation
+## Installation(#table-of-contents)
 
 What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
 
-## Usage
+## Usage(#table-of-contents)
 
 Provide instructions and examples ...
 ${data.usage}
 
-## License
-This app is covered by ${data.license}.
-## Contributing
-${data.contribution}
-## Tests
-${data.test}
-## Questions
-Any further questions don't hestiate to reach me at  to ${data.github} or via ${data.email}
+## License(#table-of-contents)
 
+ ${renderLicenseSection(data.license)}
+
+## Contributing(#table-of-contents)
+
+${data.contribution}
+
+## Tests(#table-of-conents)
+
+${data.test}
+
+## Questions(#table-of-contents)
+
+Any further questions don't hestiate to reach me at  to ${data.github} or via ${
+    data.email
+  }
+## Optional Content
+${data.content}
 `;
 }
 // module.exports = generateMarkdown;
